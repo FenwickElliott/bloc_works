d5c50a5418c902e705c39f41b1945562b52aa809
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-08-09 16:43:33 -0400

    Bug fixed, books rendering, cp2 complet

--- bloc_works/lib/bloc_works.rb
+++ bloc_works/lib/bloc_works.rb
@@ -2,11 +2,16 @@ require "bloc_works/version"
 require "bloc_works/dependencies"
 require "bloc_works/controller"
 require "bloc_works/router"
+require "bloc_works/utility"
 
 module BlocWorks
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+      controller_class, action = controller_and_action(env)
+      cont = controller_class.new(env)
+      res = cont.send(action)
+      
+      [200, {'Content-Type' => 'text/html'}, [res]]
     end
   end
 end
